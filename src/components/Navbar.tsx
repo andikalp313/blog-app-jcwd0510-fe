912ae5671669fb4f81c1822c97d54601fbfabe8c
andikalp313 <andikaluhurpambudi@gmail.com>
2024-12-09 14:24:12 +0700

    feat: keeplogin (#4)

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,24 @@
+"use client";
+
+import { useAppDispatch, useAppSelector } from "@/redux/hooks";
+import { logoutAction } from "@/redux/slices/userSlice";
 import Link from "next/link";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Navbar = () => {
+  const dispatch = useAppDispatch();
+
+  const router = useRouter();
+
+  const user = useAppSelector((state) => state.user);
+
+  const logout = () => {
+    localStorage.removeItem("blog-storage");
+    dispatch(logoutAction());
+    router.push("/login");
+  };
+
   return (
     <nav className="bg-slate-400">
       <div className="container mx-auto px-4">
@@ -10,9 +27,12 @@ const Navbar = () => {
             Logo
           </Link>
 
-          <div className="flex items-center gap-8 font-medium">
+          <div className="flex cursor-pointer items-center gap-8 font-medium">
             <Link href="/">Home</Link>
             <Link href="/">Profile</Link>
+            {!user.id && <Link href="/login">sign in</Link>}
+            {!!user.id && <p onClick={logout}> Logout</p>}
+            {/* {user.id ? <p onClick={logout}>Logout</p> : <Link href="/login">sign in</Link>} */}
           </div>
         </div>
       </div>
