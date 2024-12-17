a4a4e854245087704f37f6e4495ba88184760433
andikalp313 <andikaluhurpambudi@gmail.com>
2024-12-17 16:26:26 +0700

    Fix deploy (#13)
    * fix: deploy
    
    * fix: deploy

--- src/hooks/api/auth/useRegister.ts
+++ src/hooks/api/auth/useRegister.ts
@@ -3,6 +3,7 @@
 import { axiosInstance } from "@/lib/axios";
 import { useMutation } from "@tanstack/react-query";
 import { AxiosError } from "axios";
+import { useRouter } from "next/navigation";
 import { toast } from "react-toastify";
 
 interface RegisterPayload {
@@ -11,6 +12,7 @@ interface RegisterPayload {
   password: string;
 }
 const useRegister = () => {
+  const router = useRouter();
   return useMutation({
     mutationFn: async (payload: RegisterPayload) => {
       const { data } = await axiosInstance.post("/auth/register", payload);
@@ -18,6 +20,7 @@ const useRegister = () => {
     },
     onSuccess: () => {
       toast.success("Register Success");
+      router.push("/login");
     },
     onError: (error: AxiosError<any>) => {
       toast.error(error.response?.data);
